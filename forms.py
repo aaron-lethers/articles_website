a1e425d6fc51174fb55237ed8015670a90d384e7
aaron-lethers <69817429+aaron-lethers@users.noreply.github.com>
2022-06-02 15:08:43 -0700

    updated forms

--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms.widgets import TextArea
 
 # REGISTER FORM CLASS
 class RegisterForm(Form):
@@ -15,3 +16,13 @@ class RegisterForm(Form):
 class LoginForm(Form):
 	email = StringField('Email', [validators.input_required(), validators.Length(min=1, max=50)])
 	password = PasswordField('Password', [validators.input_required(), validators.Length(min=1, max=50)])
+
+# ARTICLE FORM CLASS
+class ArticleForm(Form):
+    title = StringField('Title', [validators.input_required(), validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.input_required(), validators.Length(min=30)])
+
+# EDIT ARTICLE FORM CLASS
+class EditForm(Form):
+    title = StringField('Title', [validators.input_required(), validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.input_required(), validators.Length(min=30)], render_kw={"rows": 10, "cols": 11})
